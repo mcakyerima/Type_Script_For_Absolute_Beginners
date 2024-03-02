14365e9849b4bfdc24f91e434e81644261ef90a3
mcakyerima <mcakyerima@gmail.com>
2024-03-02 19:05:49 +0100

    setting up tsconfig.json

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let age = 20;
+let names = "mc ak yerima";
+const display = {
+    name: names,
+    age: age,
+};
+console.log(display);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let age: number = 20;
+let names: String = "mc ak yerima";
+
+const display = {
+    name: names,
+    age: age,
+}
+
+console.log(display);
